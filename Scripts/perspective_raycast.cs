b577c0b9c06d50ebd0c03202a7464830e16347c9
DenisOSVR <denis.masek@student.gyarab.cz>
2024-01-06 18:25:33 +0100

    perspective scaling done

--- Scripts/perspective_raycast.cs
+++ Scripts/perspective_raycast.cs
@@ -5,38 +5,62 @@ public partial class perspective_raycast : RayCast3D
 {
 	bool run = true;
 	Vector3 norm;
+	float mult;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		AddException(GetNode<RigidBody3D>("/root/Root/Cube"));
+		GetNode<RayCast3D>("/root/Root/CharacterBody3D/CameraRig/RayCastCenter/RayCastTopLeft").AddException(GetNode<RigidBody3D>("/root/Root/Cube"));
+		GetNode<RayCast3D>("/root/Root/CharacterBody3D/CameraRig/RayCastCenter/RayCastTopRight").AddException(GetNode<RigidBody3D>("/root/Root/Cube"));
+		GetNode<RayCast3D>("/root/Root/CharacterBody3D/CameraRig/RayCastCenter/RayCastBotLeft").AddException(GetNode<RigidBody3D>("/root/Root/Cube"));
+		GetNode<RayCast3D>("/root/Root/CharacterBody3D/CameraRig/RayCastCenter/RayCastBotRight").AddException(GetNode<RigidBody3D>("/root/Root/Cube"));
 		GetNode<RigidBody3D>("/root/Root/Cube").Freeze = true;
+		mult = 1 / GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition.DistanceTo(GlobalPosition);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		var cPos = GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition;
-		GD.Print("Pos: " + GlobalPosition);
 		if (IsColliding() && run) {
 			var spaceState = GetWorld3D().DirectSpaceState;
+			var topLeft = GetNode<RayCast3D>("/root/Root/CharacterBody3D/CameraRig/RayCastCenter/RayCastTopLeft");
+			var topRight = GetNode<RayCast3D>("/root/Root/CharacterBody3D/CameraRig/RayCastCenter/RayCastTopRight");
+			var botLeft = GetNode<RayCast3D>("/root/Root/CharacterBody3D/CameraRig/RayCastCenter/RayCastBotLeft");
+			var botRight = GetNode<RayCast3D>("/root/Root/CharacterBody3D/CameraRig/RayCastCenter/RayCastBotRight");
+
+			var shape = GetNode<CollisionShape3D>("/root/Root/Cube/CollisionShape3D");
+			var box = GetNode<CsgBox3D>("/root/Root/Cube/CSGBox3D");
 			
-			var excl = new Godot.Collections.Array<Rid>();
-			excl.Add(GetNode<RigidBody3D>("/root/Root/Cube").GetRid());
-			//GD.Print(GlobalTransform.Origin.DistanceTo(GetCollisionPoint()));
-			var query1 = PhysicsRayQueryParameters3D.Create(GetParent<Node3D>().GlobalPosition, new Vector3(cPos.X + 0.5f, cPos.Y + 0.5f, cPos.Z + 0.5f) + new Vector3(cPos.X + 0.5f - GetParent<Node3D>().GlobalPosition.X, cPos.Y + 0.5f - GetParent<Node3D>().GlobalPosition.Y, cPos.Z + 0.5f - GetParent<Node3D>().GlobalPosition.Y).Normalized() * 100);
-			query1.Exclude = excl;
-			query1.HitFromInside = false;
-			var results = spaceState.IntersectRay(query1);
-			if (results.ContainsKey("position")) {
-				GD.Print(GlobalPosition.DistanceTo((Vector3) results["position"]));
-				GetNode<CsgSphere3D>("/root/Root/pointer1").GlobalPosition = (Vector3) results["position"];
-				//GetNode<CsgSphere3D>("/root/Root/pointer2").GlobalPosition = new Vector3(cPos.X + 0.5f, cPos.Y + 0.5f, cPos.Z + 0.5f).Normalized() * 100;
-				GetNode<CsgSphere3D>("/root/Root/pointer3").GlobalPosition = GlobalPosition;
-			} 
-			//GD.Print(results.Keys.ToString());
-			GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition = GetCollisionPoint();
+			float dist = float.MaxValue;
+
+			if (topLeft.IsColliding()) dist = topLeft.GetCollisionPoint().DistanceTo(GlobalPosition);
+			if (topRight.IsColliding()) {
+				float tmp = topRight.GetCollisionPoint().DistanceTo(GlobalPosition);
+				if (tmp < dist) dist = tmp;
+			}
+			if (botLeft.IsColliding()) {
+				float tmp = botLeft.GetCollisionPoint().DistanceTo(GlobalPosition);
+				if (tmp < dist) dist = tmp;
+			}
+			if (botRight.IsColliding()) {
+				float tmp = botRight.GetCollisionPoint().DistanceTo(GlobalPosition);
+				if (tmp < dist) dist = tmp;
+			}
+			if (dist < GetCollisionPoint().DistanceTo(GlobalPosition)) {
+				float diff = GetCollisionPoint().DistanceTo(GlobalPosition) - dist;
+				GD.Print(diff);
+				GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition = GetCollisionPoint() + (GlobalPosition - GetCollisionPoint()).Normalized() * (diff + Math.Max(box.Scale.X, Math.Max(box.Scale.Y, box.Scale.Z)) / 2);
+				/*GetNode<CollisionShape3D>("/root/Root/Cube/CollisionShape3D").Scale = new Vector3(1f, 1f, 1f) * dist * mult;
+				GetNode<CsgBox3D>("/root/Root/Cube/CSGBox3D").Scale = new Vector3(1f, 1f, 1f) * dist * mult;*/
+				shape.Scale = new Vector3(1f, 1f, 1f) * GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition.DistanceTo(GlobalPosition) * mult;
+				box.Scale = new Vector3(1f, 1f, 1f) * GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition.DistanceTo(GlobalPosition) * mult;
+			}
+			else {
+				GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition = GetCollisionPoint() + (GlobalPosition - GetCollisionPoint()).Normalized() * (Math.Max(box.Scale.X, Math.Max(box.Scale.Y, box.Scale.Z)) / 2);
+				shape.Scale = new Vector3(1f, 1f, 1f) * GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition.DistanceTo(GlobalPosition) * mult;
+				box.Scale = new Vector3(1f, 1f, 1f) * GetNode<RigidBody3D>("/root/Root/Cube").GlobalPosition.DistanceTo(GlobalPosition) * mult;
+			}
+			
 		}	
-		GetNode<CsgSphere3D>("/root/Root/pointer2").GlobalPosition = new Vector3(cPos.X + 0.5f - GlobalPosition.X, cPos.Y + 0.5f - GlobalPosition.X, cPos.Z + 0.5f - GlobalPosition.X).Normalized() * 100;
 	}
 
 	public override void _Input(InputEvent @event)
